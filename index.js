ef7ea4f587f203c69c854061aff1d9fb9dd29b24
s0LA1337 <s0la.luladust@gmail.com>
2019-11-07 00:23:59 +0100

    bugfix

--- index.js
+++ index.js
@@ -119,9 +119,16 @@ client.on("message", async message => {
 					let curCurrName = file[i].name;
 					if(curCurrName.toLowerCase().indexOf(Currency.toLowerCase()) > -1)
 					{
-						if(Currency.toLowerCase().indexOf("shard") < 0 || curCurrName.toLowerCase().indexOf("shard") < 0) { 
-							resultList.push(curCurrName);
-						}
+						if(Currency.toLowerCase().indexOf("shard") === -1) { 
+            						if(curCurrName.toLowerCase().indexOf("shard") === -1) {
+                						resultList.push(curCurrName);
+            						}
+        					}
+        					else {
+            						if(curCurrName.toLowerCase().indexOf("shard") > -1) {
+                						resultList.push(curCurrName);
+            						}
+        					}
 					}
 				}
 
