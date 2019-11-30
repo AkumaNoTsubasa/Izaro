979526f802e8bd48c71ce5d6b1ca5aa937070de3
AkumaNoTsubasa <627469+AkumaNoTsubasa@users.noreply.github.com>
2019-12-01 00:40:35 +0100

    Update index.js
    Reworked league fetch from api to allow leagues with 2 word names like "hardcore blight", added timers to current running leagues to show their time until expiration, if known.
    Added a new command ++leagues
    Fixed bug on finding multiple items within one name that returned a wrong undefined item.

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const Discord = require("discord.js");
 const fetch = require('node-fetch');
 const math = require('mathjs');
+const dateFormat = require('dateformat');
 const config = require("./config.json");
 
 const client = new Discord.Client();
+
+
 
 client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
@@ -23,7 +26,7 @@ client.on("guildDelete", guild => {
 
 client.on("message", async message => {
 
-  if(message.author.bot) return;
+  if(message.author.bot || message.guild === null) return;
   /*
   if(message.guild === null)
   {
@@ -67,7 +70,7 @@ client.on("message", async message => {
   
   if(command === "invite")
   {
-      message.channel.send('Izaro wants to see new servers as ideas for his labyrinth. Invite Izaro here: https://discordapp.com/oauth2/authorize?client_id=638458844770598914&scope=bot&permissions=392257');
+      message.channel.send('Izaro wants to see new servers as ideas for his labyrinth. Invite Izaro here: https://discordapp.com/oauth2/authorize?client_id=638458844770598914&scope=bot&permissions=392256');
   }
 
 	if(command==="support")
@@ -76,7 +79,7 @@ client.on("message", async message => {
 	}
 	if(command==="help")
 	{
-		message.channel.send('I am currently in the works and in an early alpha-status. \n\nMy current commands are: `'+config.prefix+'price`. \n\nI have also some not yet public shown commands as they might result in weird behaviour. If you have any questions, you can message `Akuma no Tsubasa#0001`\n\nIf you need further help, you can request a Serverinvite to the Support Server with `§support`.');
+		message.channel.send('I am currently in the works and in an early alpha-status. \n\nMy current commands are: `'+config.prefix+'price`, `'+config.prefix+'nlab`, `'+config.prefix+'clab`, `'+config.prefix+'mlab`, `'+config.prefix+'ulab`, . \n\nI have also some not yet public shown commands as they might result in weird behaviour. If you have any questions, you can message `Akuma no Tsubasa#0001`\n\nIf you need further help, you can request a Serverinvite to the Support Server with `'+config.prefix+'support`.');
 	}
 
 	
@@ -103,12 +106,112 @@ client.on("message", async message => {
 		else
 		{		
 			const leagues = await fetch('https://api.poe.watch/leagues').then(response => response.json());
-			if (leagues.some(element => element.name.toLowerCase() == args[0].toLowerCase()))
+			console.log(args);
+			var leaguenameX = args[0];
+			leaguenameX += " ";
+			leaguenameX += args[1];
+			var leaguenameY = args[0];
+			
+							console.log("leaguenameX: "+leaguenameX);
+							
+							
+			if (leagues.some(element => element.name.toLowerCase() == leaguenameX.toLowerCase()) && leagues.some(element => element.active == true))
+			{
+
+				message.delete(1000);
+
+				const file = await fetch('https://api.poe.watch/get?league='+leaguenameX+'&category=currency').then(response => response.json());
+				var randomColor = '#' + ("000000" + Math.random().toString(16).slice(2, 8).toUpperCase()).slice(-6);
+				var league=leaguenameX;
+				//args.slice(3, args.length);
+				Currency = args.slice(2, args.length).join(' ');
+				console.log(Currency);
+				
+				let resultList = [];
+				for(let i = 0; i < file.length; ++i) {
+					let curCurrName = file[i].name;
+					let curIcon = file[i].icon; // read icon from api
+					if(curCurrName.toLowerCase().indexOf(Currency.toLowerCase()) > -1)
+					{
+						if(Currency.toLowerCase().indexOf("shard") === -1) { 
+            						if(curCurrName.toLowerCase().indexOf("shard") === -1) {
+                						resultList.push(curCurrName);
+            						}
+        					}
+        					else {
+            						if(curCurrName.toLowerCase().indexOf("shard") > -1) {
+                						resultList.push(curCurrName);
+            						}
+        					}
+					}
+				}
+
+				if(resultList.length < 1)
+				{ 
+					const PoePriceEmbed = new Discord.RichEmbed()
+					.setColor(randomColor)
+					.setDescription("Sadly there is no currency called **" + Currency + "**. Please check the exact name incl. captital letters like `Mirror of Kalandra` instead of `mirror of kalandra`.")
+					.setTimestamp()
+					.setFooter("Requested by: " + message.author.username, message.author.avatarURL);
+					message.channel.send(PoePriceEmbed);
+				}
+				else if(resultList.length > 1) {
+					let curIndex = 0;
+					let relativeIndex; 
+					for(relativeIndex = 0; relativeIndex < 5; relativeIndex++) { 
+						if(relativeIndex > (resultList.length - curIndex)) {
+							break;
+						}
+					}
+					let results = [];
+					for(; curIndex < resultList.length; curIndex++) {
+						results.push(resultList[curIndex]);
+					}
+					let curString=""; //first item undefined if not set to any value ref XCur1
+					
+					if(resultList.length>10)
+					{
+						curString = "Too many results";
+					}
+					else
+					{
+						//results.forEach(curIcon => { curString += curIcon + " "; }); // trying to add icon here later for now output its url 
+						results.forEach(currency => { curString +=  currency + "\n"; });  
+						//First item malformatted. If search for "alt" predicted output "Orb of Alteration" and "Exalted Orb". Orb of Alteration is an undefined result. XCur1 
+					}
+					
+					const PoePriceEmbed = new Discord.RichEmbed()
+					.setColor(randomColor)
+					.setDescription("Found " + resultList.length + " items.\n" + curString)
+					.setTimestamp()
+					.setFooter("Requested by: " + message.author.username, message.author.avatarURL);
+					message.channel.send(PoePriceEmbed);
+					// Add cool reactions here
+					// Await for user reaction
+					// this code should run in a loop ! so that curIndex can increment and decrement depending on user input.
+				}
+
+				else
+				{ 
+					let curr = file.find(element => element.name.toLowerCase() === resultList[0].toLowerCase());
+					league[0] = league[0].toUpperCase();
+					const PoePriceEmbed = new Discord.RichEmbed()
+					.setColor(randomColor)
+					.setDescription("**" + curr.name + "** has currently a value of **"+ curr.median.toFixed(2) + "** Chaos Orbs or **" +
+					curr.exalted.toFixed(3) + "** Exalted Orbs in the **" + league + "** league.")
+					.setThumbnail(curr.icon)
+					.setTimestamp()
+					.setFooter("Requested by: " + message.author.username, message.author.avatarURL);
+					message.channel.send(PoePriceEmbed);
+				}
+			}
+			//Hardcore Check 
+			else if(leagues.some(element => element.name.toLowerCase() == leaguenameY.toLowerCase() && leagues.some(element => element.active == true)))
 			{
 
 				message.delete(1000);
 				
-				const file = await fetch('https://api.poe.watch/get?league='+args[0]+'&category=currency').then(response => response.json());
+				const file = await fetch('https://api.poe.watch/get?league='+leaguenameY+'&category=currency').then(response => response.json());
 				var randomColor = '#' + ("000000" + Math.random().toString(16).slice(2, 8).toUpperCase()).slice(-6);
 				var league=args[0];
 				args.shift();
@@ -145,17 +248,18 @@ client.on("message", async message => {
 				else if(resultList.length > 1) {
 					let curIndex = 0;
 					let relativeIndex; 
-					for(relativeIndex = 0; relativeIndex < 5; ++relativeIndex) { 
+					for(relativeIndex = 0; relativeIndex < 5; relativeIndex++) { 
 						if(relativeIndex > (resultList.length - curIndex)) {
 							break;
 						}
 					}
 					let results = [];
-					for(; curIndex < resultList.length - curIndex; ++curIndex) {
-						results.push(resultList[curIndex]);
+					for(; curIndex < resultList.length; curIndex++) {
+					results.push(resultList[curIndex]);
 					}
-					let curString=""; //first item undefined if not set to any value ref XCur1
-					
+					var curString=""; //first item undefined if not set to any value ref XCur1
+					console.log( results );
+					console.log( resultList );
 					if(resultList.length>10)
 					{
 						curString = "Too many results";
@@ -185,7 +289,7 @@ client.on("message", async message => {
 					const PoePriceEmbed = new Discord.RichEmbed()
 					.setColor(randomColor)
 					.setDescription("**" + curr.name + "** has currently a value of **"+ curr.median.toFixed(2) + "** Chaos Orbs or **" +
-					 	curr.exalted.toFixed(3) + "** Exalted Orbs in the **" + league + "** league.")
+					curr.exalted.toFixed(3) + "** Exalted Orbs in the **" + league + "** league.")
 					.setThumbnail(curr.icon)
 					.setTimestamp()
 					.setFooter("Requested by: " + message.author.username, message.author.avatarURL);
@@ -198,20 +302,179 @@ client.on("message", async message => {
 				var LigaListe="";
 				var LeagueName;
 				var x = 0;
+				var y = 1;
+				var now = new Date().getTime;
 				while (x < file.length)
 				{
-					//LeagueName = file[x].name;
-					LigaListe += x +": "+ file[x].name +"\n ";
-					x++;
+					if(file[x].active == true)
+					{
+						if(!file[x].end)
+						{
+							//LeagueName = file[x].name;
+							LigaListe += y +": "+ file[x].name +"\n ";
+							x++;
+							y++;
+
+						}
+						else
+						{
+
+							var leagueend = new Date(file[x].end).getTime();
+							var now = new Date().getTime();
+							var timespan = leagueend - now;
+							var days = Math.floor(timespan / (1000 * 60 * 60 * 24));
+							var hours = Math.floor((timespan % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+							var minutes = Math.floor((timespan % (1000 * 60 * 60)) / (1000 * 60));
+							var seconds = Math.floor((timespan % (1000 * 60)) / 1000);
+							LigaListe += y +": "+ file[x].name +" - Ends in: "+days+" days "+hours+" hours "+minutes+" minutes "+seconds+" seconds.\n ";
+							x++;
+							y++;
+						}
+					}
+					else
+					{
+						x++;
+					}
 				}
 				message.channel.send("Sorry, the league " + args[0] + " does not exist.\nCurrent Leagues are: \n" + LigaListe);
 			}
-		}
-		
-		
+		}	
+	}
+	
+	if(command==="leagues")
+	{
+				const file = await fetch('https://api.poe.watch/leagues').then(response => response.json());
+				var LigaListe="";
+				var LeagueName;
+				var x = 0;
+				var y = 1;
+				var now = new Date().getTime;
+				while (x < file.length)
+				{
+					if(file[x].active == true)
+					{
+						if(!file[x].end)
+						{
+							//LeagueName = file[x].name;
+							LigaListe += y +": "+ file[x].name +"\n ";
+							x++;
+							y++;
 
-		
-		
+						}
+						else
+						{
+
+							var leagueend = new Date(file[x].end).getTime();
+							var now = new Date().getTime();
+							var timespan = leagueend - now;
+							var days = Math.floor(timespan / (1000 * 60 * 60 * 24));
+							var hours = Math.floor((timespan % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+							var minutes = Math.floor((timespan % (1000 * 60 * 60)) / (1000 * 60));
+							var seconds = Math.floor((timespan % (1000 * 60)) / 1000);
+							LigaListe += y +": "+ file[x].name +" - Ends in: "+days+" days "+hours+" hours "+minutes+" minutes "+seconds+" seconds.\n ";
+							x++;
+							y++;
+						}
+					}
+					else
+					{
+						x++;
+					}
+				}
+				message.channel.send("Here is a list of all currently running leagues. Please note that *SSF* Leagues and private leagues are not listed.\nCurrent Leagues are: \n" + LigaListe);
+	}
+	
+	if(command==="ulab")
+	{
+		message.delete(1000);
+		if(dateFormat(now,"HH")<1)
+		{
+			var now = new Date();
+			now.setDate(now.getDate()-1);
+		}
+		else
+		{
+			var now = new Date();
+		}
+		const dateiname=dateFormat(now, "isoDate");
+		const ulabdatei="https://www.poelab.com/wp-content/labfiles/" + dateiname + "_uber.jpg";
+		const labfile = "https://www.poelab.com/wp-content/labfiles/uber-" + dateiname + ".json";
+		const UlabEmbed = new Discord.RichEmbed()
+		.setColor(randomColor)
+		.setDescription("Lab Compass File: " + labfile)
+		.setImage(ulabdatei)
+		.setTimestamp()
+		.setFooter("Requested by: " + message.author.username, message.author.avatarURL);
+		message.channel.send(UlabEmbed);
+	}
+	if(command==="mlab")
+	{
+		message.delete(1000);
+		if(dateFormat(now,"HH")<1)
+		{
+			var now = new Date();
+			now.setDate(now.getDate()-1);
+		}
+		else
+		{
+			var now = new Date();
+		}
+		const dateiname=dateFormat(now, "isoDate");
+		const ulabdatei="https://www.poelab.com/wp-content/labfiles/" + dateiname + "_merciless.jpg";
+		const labfile = "https://www.poelab.com/wp-content/labfiles/merciless-" + dateiname + ".json";
+		const UlabEmbed = new Discord.RichEmbed()
+		.setColor(randomColor)
+		.setDescription("Lab Compass File: " + labfile)
+		.setImage(ulabdatei)
+		.setTimestamp()
+		.setFooter("Requested by: " + message.author.username, message.author.avatarURL);
+		message.channel.send(UlabEmbed);
+	}
+	if(command==="clab")
+	{
+		message.delete(1000);
+		if(dateFormat(now,"HH")<1)
+		{
+			var now = new Date();
+			now.setDate(now.getDate()-1);
+		}
+		else
+		{
+			var now = new Date();
+		}
+		const dateiname=dateFormat(now, "isoDate");
+		const ulabdatei="https://www.poelab.com/wp-content/labfiles/" + dateiname + "_cruel.jpg";
+		const labfile = "https://www.poelab.com/wp-content/labfiles/cruel-" + dateiname + ".json";
+		const UlabEmbed = new Discord.RichEmbed()
+		.setColor(randomColor)
+		.setDescription("Lab Compass File: " + labfile)
+		.setImage(ulabdatei)
+		.setTimestamp()
+		.setFooter("Requested by: " + message.author.username, message.author.avatarURL);
+		message.channel.send(UlabEmbed);
+	}
+	if(command==="nlab")
+	{
+		message.delete(1000);
+		if(dateFormat(now,"HH")<1)
+		{
+			var now = new Date();
+			now.setDate(now.getDate()-1);
+		}
+		else
+		{
+			var now = new Date();
+		}
+		const dateiname=dateFormat(now, "isoDate");
+		const ulabdatei="https://www.poelab.com/wp-content/labfiles/" + dateiname + "_normal.jpg";
+		const labfile = "https://www.poelab.com/wp-content/labfiles/normal-" + dateiname + ".json";
+		const UlabEmbed = new Discord.RichEmbed()
+		.setColor(randomColor)
+		.setDescription("Lab Compass File: " + labfile)
+		.setImage(ulabdatei)
+		.setTimestamp()
+		.setFooter("Requested by: " + message.author.username, message.author.avatarURL);
+		message.channel.send(UlabEmbed);
 	}
 });
 client.login(config.token);
